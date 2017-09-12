4ea9e18a0128854b5f92059762a46b048dfad547
Caleb <coffincw@gmail.com>
2017-09-11 20:55:40 -0400

    finished game

--- main.py
+++ main.py
@@ -1,18 +1,68 @@
 import random
-import sys
-import os
 
 word_library = ["abyss", "habit", "welcome", "meeting", "trial", "wildfire", "quarter", "community"]
 
-print("Welcome to Hangman")
-word = word_library[random.randrange(0, 8)]
-slots = len(word)
-print("The word is %d letters long " %
-      (slots))
 
-print("What is your letter guess?")
-guess = sys.stdin.readline()
-if word.__contains__(guess):
-    print("Good guess, %s was in the word %")
+word = word_library[random.randrange(0, len(word_library))]
+answer = list(word)
+
+slots = len(word)
+gameboard = []
+for i in word:
+    gameboard.append('_')
+
+picks = []
+
+def inputGuess(guess):
+    while (len(guess) > 1 or len(guess) < 1):
+        guess = input("What is your letter guess?")
+        guess = guess.lower()
+        if (len(guess) > 1):
+            print("Please input one letter")
+        elif len(guess) < 1:
+            print("Please input a letter")
+        else:
+            return guess
+
+class Hangman():
+    rightletters = 0
+    print("Welcome to Hangman")
+    print(gameboard)
+    wrongguesses = 10
+    while (wrongguesses > 0):
+        guess = ''
+
+        print("The word is %d letters long " %
+              (slots))
+        guess = inputGuess(guess)
+
+        times = 0
+        iterate = 0
+        for i in answer:
+            iterate += 1
+            if guess == i:
+                times += 1
+                rightletters += 1
+                del gameboard[iterate - 1]
+                gameboard.insert(iterate - 1, guess)
+
+        if times > 0:
+            print("Good guess, %s was in the word %d time(s)" %
+                  (guess, times))
+
+        picks.append(guess)
+
+        if times < 1:
+            wrongguesses -= 1
+            print("Sorry, pick again! You have %d guesses left" % (wrongguesses))
+        if rightletters == slots:
+            print("Congratuations! You guessed the word '%s' correctly!" % (word))
+            break
+        else:
+            print("Game board: %s" % (gameboard))
+            print("You have guessed %s " % (picks))
+            print("--------------------------------")
+
+
 
 
